34498411da24f4a4f8b31e8d8b5d41bf8ae9a8e4
vaidehee patel <pvaidehee@gmail.com>
2021-01-25 15:56:36 -0500

    few changed for dep

---
+++ client/build/precache-manifest.0792a58f8f414f386e03500ecbcebfa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924259014d9dd275f81779a0a669be68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c2e89c6de8ab76085a",
+    "url": "/static/css/main.d0daf3c6.chunk.css"
+  },
+  {
+    "revision": "927a5e3e3109451ccc7f",
+    "url": "/static/js/2.cd129ef8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd129ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c2e89c6de8ab76085a",
+    "url": "/static/js/main.5ed8774c.chunk.js"
+  },
+  {
+    "revision": "5edf2223a9efa8ed9452",
+    "url": "/static/js/runtime-main.6a0e6603.js"
+  }
+]);
